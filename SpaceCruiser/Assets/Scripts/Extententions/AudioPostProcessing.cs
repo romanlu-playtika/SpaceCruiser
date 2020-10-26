29fd40332d1f026ff9c5732c6325df99f24181b1
romanlu <romanlu@playtika.com>
2020-10-26 17:11:47 +0300

    postprocessing and audio optimization

---
+++ SpaceCruiser/Assets/Scripts/Extententions/AudioPostProcessing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class AudioPostProcessing : AssetPostprocessor
+{
+    void OnPreprocessAudio()
+    {
+        AudioImporter audioImporter = (AudioImporter)assetImporter;
+        var settings = new AudioImporterSampleSettings();
+        var fileSize = new FileInfo(assetPath).Length / 1024;
+        if (fileSize <= 200)
+        {
+            Debug.Log($"file size is {fileSize} kb, loadtype settings: {settings.loadType}");
+            settings.loadType = AudioClipLoadType.DecompressOnLoad;
+        }
+        else if (fileSize < 5000)
+        {
+            settings.loadType = AudioClipLoadType.CompressedInMemory;
+            Debug.Log($"file size is {fileSize} kb, loadtype settings: {settings.loadType}");
+        }
+        else
+        {
+            settings.loadType = AudioClipLoadType.Streaming;
+            Debug.Log($"file size is {fileSize} kb, loadtype settings: {settings.loadType}");
+        }
+    }
+}
